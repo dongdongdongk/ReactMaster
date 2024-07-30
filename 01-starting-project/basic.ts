741ddd64b4ee552a622934ecbca2c357b8d28b59
dhk939393 <dhk9309@gmail.com>
2024-07-30 22:49:14 +0900

    type

--- 01-starting-project/basic.ts
+++ 01-starting-project/basic.ts
@@ -12,3 +12,27 @@ let isInstructor: boolean;
 
 isInstructor = true;
 
+// 배열 및 객체 유형
+
+let hobbies: string[]
+
+hobbies = ['Sports', 'Cooking', '12']
+
+
+// let person: any;
+
+let person: {
+    name: string;
+    age: number;
+}
+
+person = {
+    name: 'Max',
+    age: 32
+}
+
+// 객체 배열로 사용 
+let people: {
+    name: string;
+    age: number;
+}[];
