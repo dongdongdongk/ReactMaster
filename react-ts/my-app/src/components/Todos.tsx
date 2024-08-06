f1866ef556aa5144c3444dd6c20ad8390aa03f2d
dhk939393 <dhk9309@gmail.com>
2024-08-06 17:13:56 +0900

    Type

--- react-ts/my-app/src/components/Todos.tsx
+++ react-ts/my-app/src/components/Todos.tsx
@@ -5,13 +5,14 @@ import classes from "./Todos.module.css"
 
 type TodosProps = {
     items: Todo[];
+    onRemoveTodo: (id : string) => void;
 };
 
 const Todos: React.FC<TodosProps> = (props) => {
     return (
         <ul className={classes.todos}>
             {props.items.map((item) => (
-                <TodoItem key={item.id} text={item.text}/>
+                <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)} />
             ))}
         </ul>
     );
