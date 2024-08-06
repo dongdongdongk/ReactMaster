74526c55e8624d141025a7ca35e3f4ded613d5a7
dhk939393 <dhk9309@gmail.com>
2024-08-07 00:23:03 +0900

    hoo..

---
+++ react-ts/my-app/src/store/todos-context.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import Todo from "../models/todo"
+
+const TodosContext = React.createContext<{
+    items : Todo[];
+    addTodo : () => void;
+    removeTodo : (id : string) => void;
+}>({
+    items : [],
+    addTodo: () => {},
+    removeTodo : (id: string) => {}
+});
+
+const TodosContextProvider : React.FC = (props) => {
+    useState();
+    return <TodosContext.Provider>{props.children}</TodosContext.Provider>
+}
